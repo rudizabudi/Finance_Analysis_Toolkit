802114b92926922fc12ce501cac3abded80df470
rudizabudi <40042979+rudizabudi@users.noreply.github.com>
2019-12-15 22:21:00 +0100

    Upload

--- test4.py
+++
@@ -1,4 +0,0 @@
-import sys,os,os.path
-sys.path.append(os.path.expanduser('~/code/eol_hsrl_python'))
-os.environ['HSRL_INSTRUMENT']='gvhsrl'
-os.environ['HSRL_CONFIG']=os.path.expanduser('~/hsrl_config')
